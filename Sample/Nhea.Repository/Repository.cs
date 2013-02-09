a18825052e03e34773c2d6c75a7a9da236d47a4a
Serkan Yazıcıoğlu <serkanyazicioglu@garaj.mobi>
2013-02-09 19:30:35 +0200

    sample files

---
+++ Sample/Nhea.Repository/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Objects;
+using System.Data;
+using System.Data.Objects.DataClasses;
+using Nhea.Data;
+using System.Linq.Expressions;
+using Nhea.Enumeration;
+
+namespace Nhea.Repository
+{
+    public abstract class Repository<T> : Nhea.Data.Repository.EntityFrameworkRepository.BaseDbRepository<T> where T : class, new()
+    {
+        private System.Data.Entity.DbContext currentDbContext;
+        protected override System.Data.Entity.DbContext CurrentDbContext
+        {
+            get
+            {
+                if (currentDbContext == null)
+                {
+                    currentDbContext = new Nhea.Infrastructure.Data.NheaEntities();
+                }
+
+                return currentDbContext;
+            }
+        }
+    }
+}
