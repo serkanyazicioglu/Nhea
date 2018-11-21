7bab470699dd74da3c18fd37f0ea976a62baaa66
Nhea <serkanyazicioglu@gmail.com>
2018-11-21 11:07:20 +0300

    Some code cleanup...

--- Nhea/Configuration/Settings.Data.cs
+++ Nhea/Configuration/Settings.Data.cs
@@ -18,38 +18,6 @@ namespace Nhea.Configuration
                     return config.ConnectionName;
                 }
             }
-
-            public static string DataTextField
-            {
-                get
-                {
-                    return config.DataTextField;
-                }
-            }
-
-            public static string DataValueField
-            {
-                get
-                {
-                    return config.DataValueField;
-                }
-            }
-
-            public static bool LogDirtyFields
-            {
-                get
-                {
-                    return config.LogDirtyFields;
-                }
-            }
-
-            public static DataPreserveType DataPreserveType
-            {
-                get
-                {
-                    return config.DataPreserveType;
-                }
-            }
         }
     }
 }
