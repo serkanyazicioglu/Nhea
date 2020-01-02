7347c2f3e5b1555e92c0d56e14e71245824094ba
Serkan Yazıcıoğlu <syazicioglu@smartpulse.io>
2020-01-02 19:47:02 +0300

    Mail Queue dockerization

---
+++ Nhea.CoreCommunicationService/Services/MailQueueBackgroundService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nhea.CoreCommunicationService.Services
+{
+    public class MailQueueBackgroundService : BackgroundService
+    {
+        public MailQueueBackgroundService(ILogger<MailQueueBackgroundService> logger)
+        {
+            Logger = logger;
+        }
+
+        public ILogger<MailQueueBackgroundService> Logger { get; }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    await Task.Run(() =>
+                    {
+                        Nhea.Web.Services.ScheduledServices.MailQueueService mailQueueService = new Web.Services.ScheduledServices.MailQueueService();
+                        mailQueueService.StartService();
+
+                    }, stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogError(ex, ex.Message);
+                }
+            }
+        }
+    }
+}
--- Nhea/Configuration/GenericConfigSection/Communication/SmtpElement.cs
+++ Nhea/Configuration/GenericConfigSection/Communication/SmtpElement.cs
@@ -5,11 +5,18 @@ namespace Nhea.Configuration.GenericConfigSection.Communication
 {
     public class SmtpElement : ConfigurationElement
     {
+        private string from = null;
+
         [ConfigurationProperty("from", IsRequired = true, IsKey = true)]
         public string From
         {
             get
             {
+                if (!string.IsNullOrEmpty(from))
+                {
+                    return from;
+                }
+
                 if (!String.IsNullOrEmpty(this["from"].ToString()))
                 {
                     return this["from"].ToString();
@@ -19,13 +26,24 @@ namespace Nhea.Configuration.GenericConfigSection.Communication
                     return null;
                 }
             }
+            set
+            {
+                from = value;
+            }
         }
+
+        private string host = null;
 
         [ConfigurationProperty("host", IsRequired = true)]
         public string Host
         {
             get
             {
+                if (!string.IsNullOrEmpty(host))
+                {
+                    return host;
+                }
+
                 if (!String.IsNullOrEmpty(this["host"].ToString()))
                 {
                     return this["host"].ToString();
@@ -35,13 +53,24 @@ namespace Nhea.Configuration.GenericConfigSection.Communication
                     return null;
                 }
             }
+            set
+            {
+                host = value;
+            }
         }
+
+        private bool? enableSsl = null;
 
         [ConfigurationProperty("enableSsl", IsRequired = false)]
         public bool EnableSsl
         {
             get
             {
+                if (enableSsl.HasValue)
+                {
+                    return enableSsl.Value;
+                }
+
                 if (!String.IsNullOrEmpty(this["enableSsl"].ToString()))
                 {
                     return Convert.ToBoolean(this["enableSsl"]);
@@ -51,13 +80,24 @@ namespace Nhea.Configuration.GenericConfigSection.Communication
                     return false;
                 }
             }
+            set
+            {
+                enableSsl = value;
+            }
         }
+
+        private string userName = null;
 
         [ConfigurationProperty("userName", IsRequired = true)]
         public string UserName
         {
             get
             {
+                if (!string.IsNullOrEmpty(userName))
+                {
+                    return userName;
+                }
+
                 if (!String.IsNullOrEmpty(this["userName"].ToString()))
                 {
                     return this["userName"].ToString();
@@ -67,13 +107,24 @@ namespace Nhea.Configuration.GenericConfigSection.Communication
                     return null;
                 }
             }
+            set
+            {
+                userName = value;
+            }
         }
+
+        private string password = null;
 
         [ConfigurationProperty("password", IsRequired = true)]
         public string Password
         {
             get
             {
+                if (!string.IsNullOrEmpty(password))
+                {
+                    return password;
+                }
+
                 if (!String.IsNullOrEmpty(this["password"].ToString()))
                 {
                     return this["password"].ToString();
@@ -83,13 +134,24 @@ namespace Nhea.Configuration.GenericConfigSection.Communication
                     return null;
                 }
             }
+            set
+            {
+                password = value;
+            }
         }
+
+        private int? port = null;
 
         [ConfigurationProperty("port", IsRequired = false)]
         public int Port
         {
             get
             {
+                if (port.HasValue)
+                {
+                    return port.Value;
+                }
+
                 if (!String.IsNullOrEmpty(this["port"].ToString()))
                 {
                     return Convert.ToInt32(this["port"]);
@@ -99,13 +161,24 @@ namespace Nhea.Configuration.GenericConfigSection.Communication
                     return 25;
                 }
             }
+            set
+            {
+                port = value;
+            }
         }
+
+        private bool? isDefault = null;
 
         [ConfigurationProperty("isDefault", IsRequired = false)]
         public bool IsDefault
         {
             get
             {
+                if (isDefault.HasValue)
+                {
+                    return isDefault.Value;
+                }
+
                 if (!String.IsNullOrEmpty(this["isDefault"].ToString()))
                 {
                     return Convert.ToBoolean(this["isDefault"]);
@@ -115,40 +188,84 @@ namespace Nhea.Configuration.GenericConfigSection.Communication
                     return false;
                 }
             }
+            set
+            {
+                isDefault = value;
+            }
         }
+
+        private string defaultToRecipients = null;
 
         [ConfigurationProperty("defaultToRecipients", IsRequired = false, DefaultValue = "")]
         public string DefaultToRecipients
         {
             get
             {
+                if (!string.IsNullOrEmpty(defaultToRecipients))
+                {
+                    return defaultToRecipients;
+                }
+
                 return this["defaultToRecipients"].ToString();
             }
+            set
+            {
+                defaultToRecipients = value;
+            }
         }
+
+        private string defaultSubject = null;
 
         [ConfigurationProperty("defaultSubject", IsRequired = false, DefaultValue = "")]
         public string DefaultSubject
         {
             get
             {
+                if (!string.IsNullOrEmpty(defaultSubject))
+                {
+                    return defaultSubject;
+                }
+
                 return this["defaultSubject"].ToString();
             }
+            set
+            {
+                defaultSubject = value;
+            }
         }
+
+        private string sender = null;
 
         [ConfigurationProperty("sender", IsRequired = false, DefaultValue = "")]
         public string Sender
         {
             get
             {
+                if (!string.IsNullOrEmpty(sender))
+                {
+                    return sender;
+                }
+
                 return this["sender"].ToString();
             }
+            set
+            {
+                sender = value;
+            }
         }
+
+        private bool? autoGeneratePlainText = null;
 
         [ConfigurationProperty("autoGeneratePlainText", IsRequired = false)]
         public bool AutoGeneratePlainText
         {
             get
             {
+                if (autoGeneratePlainText.HasValue)
+                {
+                    return autoGeneratePlainText.Value;
+                }
+
                 if (!String.IsNullOrEmpty(this["autoGeneratePlainText"].ToString()))
                 {
                     return Convert.ToBoolean(this["autoGeneratePlainText"]);
@@ -157,6 +274,10 @@ namespace Nhea.Configuration.GenericConfigSection.Communication
                 {
                     return false;
                 }
+            }
+            set
+            {
+                autoGeneratePlainText = value;
             }
         }
     }
